9cd0be79077c1bf9afe244bf4a1df94383572a0e
TVU81HC <vy.truongngoclam@bosch.com>
2020-11-02 10:11:49 +0700

    updated

--- src/FrogRiverOne.java
+++ src/FrogRiverOne.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -6,8 +7,19 @@ public class FrogRiverOne {
 
     public static int solution(int X, int[] A) {
         // write your code in Java SE 8
-        HashMap<Integer, Integer> intHs = parseToMap(A);
-        return intHs.get(X) != null ? intHs.get(X) : -1;
+        HashMap<Integer , Integer> intHs = parseToMap(A);
+        Object[] B = intHs.keySet().toArray();
+        if(X > B.length) return -1;
+        int lastValue = Integer.MIN_VALUE;
+        Arrays.sort(B);
+        for(int i = 0; i < B.length; i++){
+            int value = Integer.parseInt(B[i].toString());
+            lastValue = lastValue < intHs.get(value) ? intHs.get(value) : lastValue;
+            if((i + 1 == value) && value == X){
+                return lastValue;
+            }
+        }
+        return -1;
     }
 
     public static HashMap<Integer, Integer> parseToMap (int A[]) {
@@ -20,11 +32,13 @@ public class FrogRiverOne {
     }
 
     public static void main(String args[]) {
-        int A[] = {1,3,1,4,2,3,5,4};
+        int A[] = {1,3,1,2,3,5,2};
 
         int x = 5;
 
-//        System.out.printf(""+solution(x,A));
-        System.out.printf(""+solution(5, new int[]{3}));
+        System.out.println(""+solution(3, new int[]{1, 3, 1, 3, 2, 1, 3}));
+        System.out.println(""+solution(5, new int[]{1, 3, 1, 4, 2, 3, 5, 4}));
+        System.out.println(""+solution(x,A));
+        System.out.println(""+solution(5, new int[]{3}));
     }
 }
